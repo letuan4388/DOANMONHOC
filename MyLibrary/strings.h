293bfd54b8886a6dd3f3c4a7229d578921fb2e4f
Tuan Le <Tuan Le>
2017-06-03 10:53:29 +0700

    add code

--- MyLibrary/strings.h
+++
@@ -1,8 +0,0 @@
-#ifndef LIB_STRINGS_H_
-#define LIB_STRINGS_H_
-
-extern int FindString(char src[], char str[]);
-extern void SendString(char *data);
-extern char* itoa(int i, char strnum[]);
-
-#endif /* LIB_STRINGS_H_ */
--- MyLibrary/time.h
+++
@@ -1,7 +0,0 @@
-#ifndef LIB_TIME_H_
-#define LIB_TIME_H_
-
-extern unsigned long millis(void);
-extern void DelayMS(int time);
-
-#endif /* LIB_TIME_H_ */
